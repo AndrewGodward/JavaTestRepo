cb92887bde7474894e1976b092b785c38907bed6
6234898 <andrewgod597@gmail.com>
2024-04-22 15:13:56 -0400

    add new println in the main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is a toy Project");
+        System.out.println("using Git Bash for VC");
     }
 }
